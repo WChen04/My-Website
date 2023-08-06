f340fea987d6fd16b5a2d5dca85b0f6026db5b8d
William Chen <williamchen259@gmail.com>
2023-08-05 22:59:55 -0400

    First Commits

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+import Home from "./components/Home.vue";
 </script>
 
 <template>
@@ -11,7 +12,7 @@ import HelloWorld from './components/HelloWorld.vue'
       <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
     </a>
   </div>
-  <HelloWorld msg="Vite + Vue" />
+  <Home msg="This is my Website" />
 </template>
 
 <style scoped>
