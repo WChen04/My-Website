4f3c5ff9d3c6798effd5242be120d728961462a9
William Chen <williamchen259@gmail.com>
2023-08-08 23:00:48 -0400

    Testing

--- src/App.vue
+++ src/App.vue
@@ -2,11 +2,13 @@
 import { RouterView } from "vue-router";
 import Home from "./views/Home.vue";
 import Navbar from "./components/Navbar.vue";
+import About from "./views/About.vue";
+import Contact from "./views/Contact.vue";
 </script>
 
 <template>
   <Navbar />
-  <Home msg="Hi, I'm William." />
+  <Home msg="Hello, I'm William." />
   <RouterView />
 </template>
 
